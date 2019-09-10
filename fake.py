6ab2b3ebb9ff1ab0d49a1553f35c64a0105201af
anhyunjun <42109951+anhyunjun@users.noreply.github.com>
2019-09-10 16:08:38 +0900

    first commit of blog project

---
+++ fake.py
@@ -0,0 +1,18 @@
+from faker import Faker
+
+myfake = Faker()
+myfake.seed(1)
+
+print(myfake.name())
+print(myfake.address())
+print(myfake.text())
+print(myfake.state())
+print(myfake.sentence())
+print(myfake.random_number())
+
+
+views.py DB for
+blog = Blog()
+blog.title = ""
+blog = ""
+blog.save()
